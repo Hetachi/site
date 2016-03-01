3dbcf116be381ca9ac00338634f4f0f9e63aaaf4
Hetachi <krikus6@inbox.lv>
2016-03-01 16:25:46 +0200

    Files moved to public

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 
 //app.use(express.static(__dirname + '/static'));
+app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+
 });
 
 app.listen(3000, function () {
