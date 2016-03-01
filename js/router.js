e83b06abac2f46e701ae9aa355014ea1e454e60b
Hetachi <krikus6@inbox.lv>
2016-03-01 11:59:59 +0200

    Templating system setup

---
+++ js/router.js
@@ -0,0 +1,20 @@
+define(['jquery', 'underscore', 'backbone', 'views/default'],
+function ($, _, Backbone, IndexView) {
+  var SiteRouter = Backbone.Router.extend({
+    routes: {
+      '*other': 'defaultAction'
+    }
+  });
+  var initialize = function () {
+    var siteRouter = new SiteRouter();
+    siteRouter.on('route:defaultAction', function(){
+      var defaultView = new DefaultView();
+      defaultView.render();
+      console.log('default');
+    });
+    Backbone.history.start();
+  };
+  return {
+    initialize: initialize
+  };
+});
