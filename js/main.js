233261ba7f904cfe63b4c0626fc61420367d0a50
Hetachi <krikus6@inbox.lv>
2016-03-01 10:30:44 +0200

    package managed init

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 require.config({
   paths: {
-    'jquery': '/bower_components/jquery/dist/jquery',
-    'underscore': '/bower_components/underscore/underscore',
-    'backbone': '/bower_components/backbone/backbone',
-    'mustache': '/bower_components/mustache.js/mustache',
-    'text': '/bower_components/requirejs-text/text'
+    'jquery': '../bower_components/jquery/dist/jquery',
+    'underscore': '../bower_components/underscore/underscore',
+    'backbone': '../bower_components/backbone/backbone',
+    'mustache': '../bower_components/mustache.js/mustache',
+    'text': '../bower_components/requirejs-text/text'
   },
   shim: {
     'jquery': {
